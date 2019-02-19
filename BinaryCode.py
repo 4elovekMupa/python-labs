1616ec89123c697bfa1016fc117993c55d7c2c43
4eiovekMupa <ilya.pavlov.947@gmail.com>
2019-02-19 19:05:49 +0600

    something

--- BinaryCode.py
+++ BinaryCode.py
@@ -1,15 +1,8 @@
-'''Написать функцию, принимающая 1 аргумент целое число в десятичной системе счистления,
-которая будет переводить это число в двоичную систему счистления.'''
-def funkcua (a):
-    qwerty=""
-    while a//2!=0:
-        qwerty += (str(a%2) + " ")
-        a//=2
-    qwerty += "1"
-    c = qwerty.split(" ")
-    c.reverse()
-    qwerty = "".join(c)
-    qwerty = int(qwerty)
-    return qwerty
-print (funkcua(71))
-#vudbjfvbjkdfkvzbdxfb
+def rec(a):
+    if a == 0:
+        return 1
+    print(a)
+    return rec(a-1) * a
+
+
+print(rec(7))
